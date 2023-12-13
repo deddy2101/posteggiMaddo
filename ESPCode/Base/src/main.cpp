d4d32cc735a6814402094c40dbdb20b8560a90d7
Davide Patrone <>
2023-12-13 19:08:05 +0100

    cosi si parlano

--- ESPCode/Base/src/main.cpp
+++ ESPCode/Base/src/main.cpp
@@ -6,21 +6,64 @@
 
 #define led 12
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(12, 13); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 
-struct dataStruct {
+struct dataStruct
+{
   long packetID;
   char data[32];
 };
 dataStruct data;
 
-int delayTime = 1000;
+int delayTime = 5000;
 long currentTime;
-void setup() {
+
+void handshake()
+{
+  radio.stopListening();
+  data.packetID += 1;
+  strcpy(data.data, "Ciao");
+  int i = 0;
+  while (!radio.write(&data, sizeof(data)))
+  {
+    Serial.printf("\033[1;32m[I] Trying to send packet try number %d\n\033[0m", i);
+    delay(1000);
+    i++;
+  }
+    //now the radio should respond to the handshake with "Ciao"
+  radio.startListening();
+  while (radio.available())
+  {
+    radio.read(&data, sizeof(data));
+    Serial.printf("\033[1;32m[I] PacketID: %d\n\033[0m", data.packetID);
+    Serial.printf("\033[1;32m[I] Data: %s\n\033[0m", data.data);
+    //if the data is equal to "Ciao" then continue
+    if (strcmp(data.data, "Ciao") == 0)
+    {
+      currentTime = millis();
+      break;
+    }
+  }
+  delay(1000);
+  Serial.printf("\033[1;32m[I] Packet send and ackRecived \n\033[0m");
+}
+
+void setup()
+{
   Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(addresses[1]); // 00002
+  //start the radio and check if everything is ok
+  //print the used SPI pins
+  delay(2000);
+    Serial.printf("\033[1;32m[I] SPI pins: SCK = %d, MISO = %d, MOSI = %d, SS = %d\n\033[0m", SCK, MISO, MOSI, SS);\
+
+  while (!radio.begin()) {
+    Serial.printf("\033[1;31m[E] Error starting the radio trying again\n\033[0m");
+    delay(1000);
+  }
+  Serial.printf("\033[1;32m[I] Radio started\n\033[0m");
+  
+  radio.openWritingPipe(addresses[1]);    // 00002
   radio.openReadingPipe(1, addresses[0]); // 00001
   radio.setPALevel(RF24_PA_MIN);
   data.packetID = 0;
@@ -28,30 +71,25 @@ void setup() {
   currentTime = millis();
 }
 
-void loop() {
-  if (millis() - currentTime >= delayTime) {
+void loop()
+{
+  //run the handshake every 5 seconds
+  if (millis() - currentTime > delayTime)
+  {
+    //print that we are starting the handshake in the serial monitor orange
+    Serial.printf("\033[1;33m[W] Starting handshake\n\033[0m");
     handshake();
+    currentTime = millis();
   }
-  while(radio.available()) {
+  while (radio.available())
+  {
     radio.read(&data, sizeof(data));
     Serial.printf("\033[1;32m[I] PacketID: %d\n\033[0m", data.packetID);
     Serial.printf("\033[1;32m[I] Data: %s\n\033[0m", data.data);
-    //if data is equal to "Ciao" then reset the currentTime
-    if (strcmp(data.data, "Ciao") == 0) {
+    // if data is equal to "Ciao" then reset the currentTime
+    if (strcmp(data.data, "Ciao") == 0)
+    {
       currentTime = millis();
     }
   }
-}
-
-
-void handshake() {
-  radio.stopListening();
-  data.packetID += 1;
-  strcpy(data.data, "Ciao"); 
-  while(!radio.write(&data, sizeof(data))) {
-            Serial.printf("\033[1;32m[I] Trying to send packet\n\033[0m");
-  }
-  Serial.printf("\033[1;32m[I] Packet send and ackRecived \n\033[0m");
-  radio.startListening();
-
 }
--- ESPCode/CANCELLO/src/main.cpp
+++ ESPCode/CANCELLO/src/main.cpp
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
+#include <Arduino.h>
 #define button 4
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(12, 13); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 
 struct dataStruct {
@@ -20,17 +20,19 @@ void handshake() {
   radio.startListening();
   while(radio.available()) {
     radio.read(&data, sizeof(data));
-    Serial.printf("\033[1;32m[I] PacketID: %d\n\033[0m", data.packetID);
-    Serial.printf("\033[1;32m[I] Data: %s\n\033[0m", data.data);
+    Serial.printf("\033[1;33m[W] PacketID: %d\n\033[0m", data.packetID);
+    Serial.printf("\033[1;33m[W] Data: %s\n\033[0m", data.data);
     //if the data is equal to "Ciao" then send back "Ciao"
     if (strcmp(data.data, "Ciao") == 0) {
-      data.packetID += 1;
       strcpy(data.data, "Ciao");
       radio.stopListening();
+      int i = 0;
       while(!radio.write(&data, sizeof(data))) {
-        Serial.printf("\033[1;32m[I] Trying to send packet\n\033[0m");
+        Serial.printf("\033[1;33m[W] Trying to send packet try number %d\n\033[0m", i);
+        i++;
+        delay(500);
       }
-      Serial.printf("\033[1;32m[I] Packet send and ackRecived \n\033[0m");
+      Serial.printf("\033[1;33m[W] Packet send and ackRecived \n\033[0m");
       radio.startListening();
     }
   }
@@ -39,21 +41,45 @@ void handshake() {
 
 
 void setup() {
-  pinMode(button, INPUT);
+  Serial.begin(9600);
+  //start the radio and check if everything is ok
+  //print the used SPI pins
+  delay(2000);
+    Serial.printf("\033[1;32m[I] SPI pins: SCK = %d, MISO = %d, MOSI = %d, SS = %d\n\033[0m", SCK, MISO, MOSI, SS);\
+
+  while (!radio.begin()) {
+    Serial.printf("\033[1;31m[E] Error starting the radio trying again\n\033[0m");
+    delay(1000);
+  }
+  Serial.printf("\033[1;32m[I] Radio started\n\033[0m");
   radio.begin();
   radio.openWritingPipe(addresses[0]); // 00001
   radio.openReadingPipe(1, addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
   handshake();
   currentTime = millis();
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
   delay(1000);
   while(radio.available()) {
     radio.read(&data, sizeof(data));
-    Serial.printf("\033[1;32m[I] PacketID: %d\n\033[0m", data.packetID);
-    Serial.printf("\033[1;32m[I] Data: %s\n\033[0m", data.data);
+    if (strcmp(data.data, "Ciao") == 0) { //the packet is a handshake
+      strcpy(data.data, "Ciao");
+      radio.stopListening();
+      int i = 0;
+      while(!radio.write(&data, sizeof(data))) {
+        Serial.printf("\033[1;33m[W] Trying to send packet try number %d\n\033[0m", i);
+        i++;
+        delay(500);
+      }
+      Serial.printf("\033[1;33m[W] Packet send and ackRecived \n\033[0m");
+      radio.startListening();
+    }
+    Serial.printf("\033[1;33m[W] PacketID: %d\n\033[0m", data.packetID);
+    Serial.printf("\033[1;33m[W] Data: %s\n\033[0m", data.data);
     //if data is equal to "Ciao" then reset the currentTime
   }
 }
